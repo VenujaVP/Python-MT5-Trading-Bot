fafa1511abf1aa8cbd8ecbcebfc399e21f720a1c
Venuja Prasanjith <144810283+VenujaVP@users.noreply.github.com>
2025-03-24 14:46:36 +0530

    Update 03

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 import MetaTrader5 as mt5
 import datetime
+from tabulate import tabulate
 mt5.initialize(login = 5032895927,password = "1mMtR-Mb", server = "MetaQuotes-Demo")
 
 # print(mt5.version())
+
+#-----------------------------------------------------------------------------
+# 1. Market Data
 
 # get symbols list and information
 symbols = mt5.symbols_get()
@@ -20,3 +24,42 @@ symbol_info = mt5.symbol_info("EURUSD")
 # Retrieves historical price data (OHLCV) for a symbol
 rates = mt5.copy_rates_from("EURUSD", mt5.TIMEFRAME_M1, datetime.datetime.now(), 100)
 # print(rates)
+
+
+#-----------------------------------------------------------------------------
+# 2. Orders and Trades
+
+# Retrieves a list of pending orders.
+orders = mt5.orders_get()
+print("Retrieves a list of pending orders.")
+if orders:
+    order_list = []
+    for order in orders:
+        order_list.append([
+            order.ticket, order.symbol, order.volume_current, order.price_open,
+            order.type, order.magic, order.comment
+        ])
+
+    headers = ["Ticket", "Symbol", "Volume", "Price", "Type", "Magic", "Comment"]
+    print(tabulate(order_list, headers=headers, tablefmt="grid"))
+else:
+    print("No pending orders found.")
+
+
+# Retrieves a list of open positions.
+positions = mt5.positions_get()
+print("\nRetrieves a list of open positions.")
+if positions:
+    position_list = []
+    for position in positions:
+        position_list.append([
+            position.ticket, position.symbol, position.volume, position.price_open,
+            position.price_current, position.type, position.profit, position.comment
+        ])
+
+    headers = ["Ticket", "Symbol", "Volume", "Open Price", "Current Price", "Type", "Profit", "Comment"]
+    print(tabulate(position_list, headers=headers, tablefmt="grid"))
+else:
+    print("No open positions found.")
+
+# 
