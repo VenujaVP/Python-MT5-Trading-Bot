3a6169c0919d0c2831a35780527f101b9ae780eb
Venuja Prasanjith <144810283+VenujaVP@users.noreply.github.com>
2025-03-29 16:01:48 +0530

    Enhance MT5 initialization and implement multi-threaded Bot execution for improved performance

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 import MetaTrader5 as mt5
 from classes import Bot
-import pandas as pd
+from threading import Thread
 
 
-mt5.initialize(login = 5032895927,password = "1mMtR-Mb", server = "MetaQuotes-Demo")
-
+# Initialize MT5 properly
+if not mt5.initialize(login=5032895927, password="1mMtR-Mb", server="MetaQuotes-Demo"):
+    print("Failed to initialize MT5")
+    mt5.shutdown()
+    exit()
 
 bot1 = Bot('EURUSD', 0.01, 10, 1)
-bot1 = Bot('GBPUSD', 0.01, 10, 1)
-bot1 = Bot('USDCAD', 0.01, 10, 1)
+bot2 = Bot('GBPUSD', 0.01, 10, 1)
+bot3 = Bot('USDCAD', 0.01, 10, 1)
+
+# meheme demm run wenne nee object 3 m
+# bot1.run()
+# bot2.run()
+# bot3.run()
+
+def b1():
+    bot1.run()
+
+def b2():
+    bot2.run()
+
+def b3():
+    bot3.run()
 
 
-bot1.run()
+thread1 = Thread(target=b1)
+thread2 = Thread(target=b2)
+thread3 = Thread(target=b3)
+
+thread1.start()
+thread2.start()
+thread3.start()
+
